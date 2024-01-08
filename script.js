841f3646d0f038a991f9d9bfeaef227c1f8ec286
Demonik993 <wiecek.dom@gmail.com>
2024-01-08 19:29:04 +0100

    constructor functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//constuctor function
+const Person = function (firstName, birthDate) {
+  this.firstName = firstName;
+  this.birthDate = birthDate;
+  // never do this:!!
+  //   this.calcAge = function () {
+  //     console.log(new Date().getFullYear() - this.birthDate);
+  //   };
+};
+// 1. new {} is created
+// 2. function is called
+// 3.  {} is linked to prototype
+// 4.function automatically return {}
+const jan = new Person('Jan', 1939);
+const dom = new Person('Dom', 1993);
+const adam = new Person('Adam', 0);
+console.log(jan, dom, adam);
+console.log(jan instanceof Person);
